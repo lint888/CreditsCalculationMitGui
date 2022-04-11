22d7d75ee06070466e494dbc62de2da0ff6d4806
lint888 <lintsun1981@gmail.com>
2022-04-11 15:07:03 +0200

    changes, menubar

--- Start.py
+++ Start.py
@@ -1,38 +1,49 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import QSize, Qt
+import sys
 
 class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, parent=None):
+        super().__init__(parent)
         self.setWindowTitle("Credits Calculation")
-        self.setFixedSize(QSize(500,800))
-        button = QPushButton("press this button",self)
-        button.setCheckable(True)
-        self.label = QLabel()
+        self.resize(500,600)
+        self.createMenu()
 
+        #create the label for credits input and the input area
+        self.Creditslabel = QLabel(self)
+        self.Creditslabel.setText("Credits")
+        self.Creditslabel.move(100,50)
         self.input = QLineEdit(self)
-        self.input.setVisible(True)
-        self.input.textChanged.connect(self.label.setText)
-
-        layout = QVBoxLayout()
-        layout.addWidget(self.input)
-        layout.addWidget(self.label)
-
-        container = QWidget()
-        container.setLayout(layout)
+        self.input.move(200,50)
+        #create the label for grade input
+        self.gradelabel = QLabel(self)
+        self.gradelabel.setText("Grade")
+        self.gradelabel.move(100,100)
+        self.inputgrade = QLineEdit(self)
+        self.inputgrade.move(200,100)
 
 
 
-        button.clicked.connect(self.ButtonClicked)
-        #button.clicked.connect(self.disabletheWindow) disable the whole window
-
-    def ButtonClicked(self):
-        print("clicked")
-        #self.setWindowTitle("Window disabled")
-    def disabletheWindow(self):
-        self.setDisabled(True)
+        self.Resultlabel = QLabel(self)
+        self.Resultlabel.setText("Result")
+        self.Resultlabel.move(100,200)
+        #self.Resultlabel.setText(self.input.text())
 
 
+    def createMenu(self):
+        menuBar = self.menuBar()
+        menuBar.setNativeMenuBar(False)
+        # File menu
+        fileMenu = QMenu(" &File", self)
+        menuBar.addMenu(fileMenu)
+        # Open Recent submenu
+        self.openRecentMenu = fileMenu.addMenu("Open Recent")
+        self.readImages = fileMenu.addMenu("Read Images")
+        # Separator
+        fileMenu.addSeparator()
+
+        editMenu = menuBar.addMenu(" &Edit")
+        menuBar.addMenu(editMenu)
 
 
 
@@ -40,9 +51,12 @@ class MainWindow(QMainWindow):
 
 
 
-app = QApplication([])
 
-window = MainWindow()
-window.show()
 
-app.exec()
+
+if __name__=='__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+
+    window.show()
+    sys.exit(app.exec())
